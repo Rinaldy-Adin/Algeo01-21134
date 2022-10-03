14004a942991faeb73f0ebe3e251bf5ea11caf5d
Rinaldy Adin <16521390@mahasiswa.itb.ac.id>
2022-10-03 19:54:27 +0700

    Test Linear Regression

--- src/test/main/LinearRegressionTest.java
+++ src/test/main/LinearRegressionTest.java
@@ -1,4 +1,62 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import tubes.matrix.LinearRegression;
+import tubes.matrix.Matrix;
+import tubes.util.Util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class LinearRegressionTest {
+
+    @Test
+    void testApprox1() {
+        try {
+            double[][] samplex = Util.readFromFile(new File("src/test/cases/linearregression/linreg1x.txt").getCanonicalPath());
+            double[][] sampley = Util.readFromFile(new File("src/test/cases/linearregression/linreg1y.txt").getCanonicalPath());
+            double[][] samplek = {{50, 76, 29.3}};
+            Matrix x = Matrix.transpose(new Matrix(samplex));
+            Matrix y = new Matrix(sampley);
+            Matrix k = new Matrix(samplek);
+
+            double expected = 0.94106;
+            assertEquals(expected, LinearRegression.approxMLR(x, y, k), 1e-2);
+        } catch (IOException e) {
+            fail(e);
+        }
+    }
+
+    @Test
+    void testEquation1() {
+        try {
+            double[][] samplex = Util.readFromFile(new File("src/test/cases/linearregression/linreg1x.txt").getCanonicalPath());
+            double[][] sampley = Util.readFromFile(new File("src/test/cases/linearregression/linreg1y.txt").getCanonicalPath());
+            Matrix x = Matrix.transpose(new Matrix(samplex));
+            Matrix y = new Matrix(sampley);
+
+            String expected = "f(X1, X2, X3) = -3.508 - 0.003 X1 + 0.001 X2 + 0.154 X3";
+            assertEquals(expected, LinearRegression.writeMLREquation(x, y));
+        } catch (IOException e) {
+            fail(e);
+        }
+    }
+
+    @Test
+    void testEquation2() {
+        try {
+            double[][] samplex = Util.readFromFile(new File("src/test/cases/linearregression/linreg2x.txt").getCanonicalPath());
+            double[][] sampley = Util.readFromFile(new File("src/test/cases/linearregression/linreg2y.txt").getCanonicalPath());
+            Matrix x = Matrix.transpose(new Matrix(samplex));
+            Matrix y = new Matrix(sampley);
+
+            String expected = "f(X1, X2) = 3.919 + 2.491 X1 - 0.466 X2";
+            assertEquals(expected, LinearRegression.writeMLREquation(x, y));
+        } catch (IOException e) {
+            fail(e);
+        }
+    }
 }
--- src/tubes/matrix/LinearRegression.java
+++ src/tubes/matrix/LinearRegression.java
@@ -7,21 +7,21 @@ public class LinearRegression {
     // H : matrix of sum of product of regressors (xi) and dependent variables (y)
     // aug : augmented matrix of a|h
 
-    public static Matrix getMtxA (Matrix x) {
+    public static Matrix getMtxA(Matrix x) {
         // x : Matrix of regressors 
         // [ [X1_1, X1_2, ... X1_m]
         //   [X2_1, X2_2, ... X2_m]
         //    ...   ...   ...  ...
         //   [Xn_1, Xn_2, ... Xn_m] ]
 
-        Matrix a = new Matrix(x.nRows+1, x.nRows+1);
+        Matrix a = new Matrix(x.nRows + 1, x.nRows + 1);
         int i, j, k;
 
         // Assign value to Matrix a
-        for (i=0; i<a.nRows; i++) {
-            for (j=0; j<a.nCols; j++) {
+        for (i = 0; i < a.nRows; i++) {
+            for (j = 0; j < a.nCols; j++) {
                 // First element m
-                if (i==0 && j==0) {
+                if (i == 0 && j == 0) {
                     a.data[0][0] = x.nCols;
                 }
 
@@ -29,12 +29,11 @@ public class LinearRegression {
                 // First column : Sigma(Xj)
                 else if (i == 0 || j == 0) {
                     double sum = 0;
-                    for (k=0; k<x.nCols; k++) {
+                    for (k = 0; k < x.nCols; k++) {
                         if (i == 0) {
-                            sum += x.data[j-1][k];
-                        }
-                        else if (j == 0) {
-                            sum += x.data[i-1][k];
+                            sum += x.data[j - 1][k];
+                        } else if (j == 0) {
+                            sum += x.data[i - 1][k];
                         }
                     }
                     a.data[i][j] = sum;
@@ -43,18 +42,18 @@ public class LinearRegression {
                 // {Xij | i>0, j>0} : Sigma(Xik*Xjk)
                 else {
                     double sumOfProduct = 0;
-                    for (k=0; k<x.nCols; k++) {
-                        sumOfProduct += x.data[i-1][k]*x.data[j-1][k];
+                    for (k = 0; k < x.nCols; k++) {
+                        sumOfProduct += x.data[i - 1][k] * x.data[j - 1][k];
                     }
                     a.data[i][j] = sumOfProduct;
                 }
             }
         }
-        
+
         return a;
     }
 
-    public static Matrix getMtxH (Matrix x, Matrix y) {
+    public static Matrix getMtxH(Matrix x, Matrix y) {
         // x : Matrix of regressors 
         // [ [X1_1, X1_2, ... X1_m]
         //   [X2_1, X2_2, ... X2_m]
@@ -67,72 +66,67 @@ public class LinearRegression {
         //   ...
         //   [Ym] ]
 
-        Matrix h = new Matrix(x.nRows+1, 1);
+        Matrix h = new Matrix(x.nRows + 1, 1);
         int i, j;
 
         // Initialize first element to be Sigma(y)
         double sumY = 0;
-        for (j=0; j<y.nRows; j++) {
+        for (j = 0; j < y.nRows; j++) {
             sumY += y.data[j][0];
         }
         h.data[0][0] = sumY;
 
         // Sum of Product retrieved by Matrix Multiplication
         Matrix sumOfProduct = Matrix.multiplyMatrix(x, y);
-        for (i=1; i<h.nRows; i++) {
-            h.data[i][0] = sumOfProduct.data[i-1][0];
+        for (i = 1; i < h.nRows; i++) {
+            h.data[i][0] = sumOfProduct.data[i - 1][0];
         }
 
         return h;
     }
 
-    public static Matrix multipleLinearReg (Matrix x, Matrix y) {
+    public static Matrix multipleLinearReg(Matrix x, Matrix y) {
         // Solve for b with Gauss-Jordan Elimination
-        Matrix aug = new Matrix (Matrix.makeAugmented(LinearRegression.getMtxA(x), LinearRegression.getMtxH(x, y)).data.clone());
+        Matrix aug = new Matrix(Matrix.makeAugmented(LinearRegression.getMtxA(x), LinearRegression.getMtxH(x, y)).data.clone());
         return LinearEquation.gaussJordanElimination(aug);
     }
 
-    public static String writeMLREquation (Matrix x, Matrix y) {
+    public static String writeMLREquation(Matrix x, Matrix y) {
         Matrix aug = LinearRegression.multipleLinearReg(x, y);
-        double[] solution = aug.getColAsArray(aug.nCols-1);
+        double[] solution = aug.getColAsArray(aug.nCols - 1);
         int i;
-        
+
         String outputString = "f(";
-        for (i=1; i<aug.getNCols()-1; i++) {
+        for (i = 1; i < aug.getNCols() - 1; i++) {
             outputString += "X" + (i);
-            if (i < aug.getNCols()-2) {
+            if (i < aug.getNCols() - 2) {
                 outputString += ", ";
-            }
-            else {
+            } else {
                 outputString += ")";
             }
         }
         outputString += " = ";
 
-        for (i=0; i<solution.length; i++) {
+        for (i = 0; i < solution.length; i++) {
             if (solution[i] != 0) {
                 if (i == 0) {
                     outputString += String.format("%.3f", (solution[i]));
-                }
-                else {
+                } else {
                     outputString += String.format("%.3f", Math.abs(solution[i]));
                     outputString += " X" + (i);
                 }
-                
-                if (i!=solution.length-1) {
-                    if (solution[i+1] > 0) {
+
+                if (i != solution.length - 1) {
+                    if (solution[i + 1] > 0) {
                         outputString += " + ";
-                    }
-                    else if (solution[i+1] < 0) {
+                    } else if (solution[i + 1] < 0) {
                         outputString += " - ";
                     }
                 }
-            }
-            else if (solution[i] == 0 && i != 0 && i != solution.length-1) {
-                if (solution[i+1] > 0) {
+            } else if (solution[i] == 0 && i != 0 && i != solution.length - 1) {
+                if (solution[i + 1] > 0) {
                     outputString += " + ";
-                }
-                else if (solution[i+1] < 0) {
+                } else if (solution[i + 1] < 0) {
                     outputString += " - ";
                 }
             }
@@ -140,13 +134,13 @@ public class LinearRegression {
         return outputString;
     }
 
-    public static double approxMLR (Matrix x, Matrix y, Matrix k) {
+    public static double approxMLR(Matrix x, Matrix y, Matrix k) {
         // k : Matrix of x values that are to be approximated
         Matrix aug = LinearRegression.multipleLinearReg(x, y);
-        double[] solution = aug.getColAsArray(aug.nCols-1);
+        double[] solution = aug.getColAsArray(aug.nCols - 1);
         double sumY = solution[0];
-        for (int i=1; i<solution.length; i++) {
-            sumY += solution[i]*k.data[0][i-1];
+        for (int i = 1; i < solution.length; i++) {
+            sumY += solution[i] * k.data[0][i - 1];
         }
         return sumY;
     }
